da983430636e54f27e9412fdff1bb730c3524c46
user name <user email>
2022-11-26 16:44:19 -0800

    Initial commi

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CartReducer from "./CartRedux";
+import UserSlice from "./UserSlice";
 
 export default configureStore({
   reducer: {
     cart: CartReducer,
+    user: UserSlice,
   },
 });
